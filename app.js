9d9de344533b789fb624e75b25fb481273b975a0
zethrobel <zethrobel@gmail.com>
2024-10-15 11:44:12 +0300

    vercel app dated

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "https://robel-news.onrender.com", // Update with your frontend URL
+    origin: "https://news-robel.vercel.app", // Update with your frontend URL
     credentials: true // Allow credentials to be shared
 }));
 
